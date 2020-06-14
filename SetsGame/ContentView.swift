a966221fd13726e173d4333e6cd8303e6dbe0324
Hadi Laasi <hadi.laasi@gmail.com>
2020-06-14 19:31:27 +0100

    Initial Commit

---
+++ SetsGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SetsGame
+//
+//  Created by Hadi Laasi on 14/06/2020.
+//  Copyright © 2020 Hadi Laasi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
